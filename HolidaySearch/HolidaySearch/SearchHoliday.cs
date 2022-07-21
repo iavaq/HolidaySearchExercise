f10004895d99db61bfa4963f1a53847a49ff4a9e
Ia <iavaquilar@gmail.com>
2022-07-21 21:30:37 +0100

    Defined SearchHoliday class

---
+++ HolidaySearch/HolidaySearch/SearchHoliday.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidaySearch
+{
+    public class SearchHoliday
+    {
+        public string DepartingFrom { get; set; }
+        public string TravellingTo { get; set; }
+        public string DepartureDate { get; set; }
+        public int Duration { get; set; }
+
+    }
+}
---
+++ HolidaySearch/HolidaySearchTests/SearchHolidayTests.cs
@@ -0,0 +1,22 @@
+﻿using HolidaySearch.Models;
+using HolidaySearch;
+using FluentAssertions;
+
+namespace HolidaySearchTests
+{
+    public class SearchHolidayTests
+    {
+        private SearchHoliday search;
+
+        [SetUp]
+        public void Setup()
+        {
+            search = new SearchHoliday();
+        }
+
+        [Test]
+        public void Test1()
+        { 
+        }
+    }
+}

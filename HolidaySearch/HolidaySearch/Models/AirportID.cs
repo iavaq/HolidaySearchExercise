d40d6474fdc7f2a372170c75b1394bef3d9ec13d
Ia <iavaquilar@gmail.com>
2022-07-21 21:39:48 +0100

    Defined AirportID Enum

---
+++ HolidaySearch/HolidaySearch/Models/AirportID.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidaySearch.Models
+{
+    public enum AirportID
+    {
+        MAN,
+        AGP,
+        PMI,
+        LPA,
+        TFS,
+        LTN,
+        LGW,
+    }
+}

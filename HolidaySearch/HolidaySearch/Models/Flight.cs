5105f7ff1f7be299af1d406f8c002667c11b42da
Ia <iavaquilar@gmail.com>
2022-07-21 16:23:29 +0100

    1. FlightData.json and HotelData.json added to project 2. Flight and Hotel objects defined

---
+++ HolidaySearch/HolidaySearch/Models/Flight.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidaySearch.Models
+{
+    public class Flight
+    { 
+        /* SAMPLE FLIGHT OBJECT
+        "id": 1,
+        "airline": "First Class Air",
+        "from": "MAN",
+        "to": "TFS",
+        "price": 470,
+        "departure_date": "2023-07-01"
+        */
+
+        public int Id { get; private set; }
+        public string Airline { get; private set; }
+        public string DepartureID { get; private set; }
+        public string ArrivalID  { get; private set; }
+        public decimal Price   { get; private set; }
+        public DateOnly DepartureDate   { get; private set; }
+
+     }
+}
---
+++ HolidaySearch/HolidaySearch/Models/Hotel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidaySearch.Models
+{
+    public class Hotel
+    {
+        /* SAMPLE Hotel Object
+        "id": 1,
+        "name": "Iberostar Grand Portals Nous",
+        "arrival_date": "2022-11-05",
+        "price_per_night": 100,
+        "local_airports": ["TFS"],
+        "nights": 7
+        */
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public DateOnly Arrival { get; private set; }
+        public decimal PricePerNight { get; private set; }
+        public List<string> LocalAirports { get; private set; }
+        public int NumberOfNights { get; private set; }
+
+    }
+}
